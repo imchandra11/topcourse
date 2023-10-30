a9369e440fdf87452b966e11d718ced92003b465
chandra prakash <chandra385123@gmail.com>
2023-10-30 11:42:20 +0530

    first commit

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,53 @@
+import Card from "./Card";
+import React, { useState } from 'react'
+
+const Cards = (props) => {
+    // console.log(props.category);
+    console.log(typeof(props.courses));
+
+    let category = props.category;
+    const [likedCourses, setLikedCourses] = useState([]);
+    //   let allCourse = [];
+
+    // It Returns list of all courses received from the api Response
+
+    // const getCourses = () => {
+    //     Object.values(props.courses).forEach((courseCategory) => {
+    //         courseCategory.forEach((course) => {
+    //             allCourse.push(course);
+    //         });
+    //     });
+    //     return allCourse;
+    // };
+
+    function getCourses() {
+        if (category === "All") {
+            let allCourses = [];
+            Object.values(props.courses).forEach((array) => {
+                array.forEach((courseData) => {
+                    allCourses.push(courseData);
+                });
+            });
+            return allCourses;
+        }
+        else
+        {
+            return props.courses[category];
+        }
+    }
+
+    //   console.log(allCourse);
+    console.log(getCourses());
+    return (
+        <div className="flex flex-wrap justify-center gap-4 mb-4">
+            {
+                Object.values(props.courses).map((course) => {
+                    return( <Card course={course}  likedCourses={likedCourses} setLikedCourses={setLikedCourses} />);
+                })
+            }
+           
+        </div>
+    );
+};
+
+export default Cards;
